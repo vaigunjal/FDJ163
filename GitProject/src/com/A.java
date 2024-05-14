34a07995de0a767d718ec9fa7c4bc90975b9d8a4
vaishnavi <vaishnavigunjal26@gmail.com>
2024-05-14 11:03:44 +0530

    by cjc

--- GitProject/src/com/A.java
+++ GitProject/src/com/A.java
@@ -1,6 +1,12 @@
 package com;
 
 public class A {
+	
+	public void m1()
+	{
+		System.out.println("M1");
+	}
+	
   public static void main(String[] args) {
 	System.out.println("This is GitProject");
 }
